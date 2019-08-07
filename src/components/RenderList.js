dfeed4e597127ae5c206306c29602329281324d9
gadgetvala <surajj.verma7@gmail.com>
2019-08-07 20:23:03 +0530

    Add checkbok strike : Bug - strike apply to all

--- src/components/RenderList.js
+++ src/components/RenderList.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
 import "./css/RenderList.css";
 
 const RenderList = props => {
 
+	const [ isChecked ,setisChecked ] = useState(false);
+	const [ addStrike ,setaddStrike ] = useState("");
+
+	//Function : For adding and remove Strike
+	const checkStrike = () => {
+		if(isChecked === false) {
+			setaddStrike("addStrike");
+			setisChecked(true);
+		}else {
+			setaddStrike("");
+			setisChecked(false);
+		}
+	}
+
+	//Function : Passing the list id for removing the item
 	const passDeleteID = (id) => {
 		props.deleteItem(id)
 	}
@@ -13,13 +28,18 @@ const RenderList = props => {
 				{props.lists.map( item => {
 					return(
 						<li key={item.id}>
-								<input type="checkbox" id="fruit1" name="fruit-1" value="Apple"/>
-								<label>{item.value}</label>															
-								<a className="round-button"
-									onClick={() => passDeleteID(item.id)}
-								>
-									X
-								</a>
+							<input 
+								defaultChecked={isChecked} 
+								onChange={checkStrike} 
+								type="checkbox" 
+								value="Apple"
+							/>
+							<label className={`labelFormat ${addStrike}`}>{item.value}</label>															
+							<a className="round-button"
+								onClick={() => passDeleteID(item.id)}
+							>
+								X
+							</a>
 						</li>
 					)
 				})}
